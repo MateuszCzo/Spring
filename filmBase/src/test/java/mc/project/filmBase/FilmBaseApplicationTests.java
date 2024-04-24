e80ad59d9f3516382b923601ce541a5b289a9607
Mateusz Czosnyka <mateuszczosn@gmail.com>
2024-04-24 09:17:25 +0200

    film base - inti

---
+++ filmBase/src/test/java/mc/project/filmBase/FilmBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package mc.project.filmBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
