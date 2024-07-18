ebeb7dd75b7a9a6734989381f8d05539fd2dd11d
Mateusz Czosnyka <mateuszczosn@gmail.com>
2024-07-18 21:50:54 +0200

    docker implementation

---
+++ online_store_with_chatbot/online_store/src/main/java/mc/project/online_store/TestController.java
@@ -0,0 +1,14 @@
+package mc.project.online_store;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("OK");
+    }
+}
