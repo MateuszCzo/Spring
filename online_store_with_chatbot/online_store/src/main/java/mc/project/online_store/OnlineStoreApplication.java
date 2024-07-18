294f0d40283a8b88fad827c49c7d6031666a49e1
Mateusz Czosnyka <mateuszczosn@gmail.com>
2024-07-18 20:42:15 +0200

    online store init

---
+++ online_store_with_chatbot/online_store/src/main/java/mc/project/online_store/OnlineStoreApplication.java
@@ -0,0 +1,11 @@
+package mc.project.online_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStoreApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStoreApplication.class, args);
+	}
+}
---
+++ online_store_with_chatbot/online_store/src/test/java/mc/project/online_store/OnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package mc.project.online_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
