caf73c4acc841bf212e736f505903d5437cf82ac
Mateusz Czosnyka <mateuszczosn@gmail.com>
2024-09-23 21:46:36 +0200

    chatbot v2 (gpt) init

---
+++ online_store_with_chatbot/chatbot_v2/README.md
@@ -0,0 +1,20 @@
+Commands to run chatbot:
+
+apt-get update
+apt-get install curl
+apt-get install python3
+apt-get install python3-pip
+apt install python3-venv
+
+curl -fsSL https://ollama.com/install.sh | sh
+
+ollama serve
+ollama pull mxbai-embed-large
+ollama pull llama3.1
+
+python3 -m venv chatbot-env
+source chatbot-env/bin/activate (run "deactivate" command to leave)
+
+pip3 install --no-cache-dir -r requirements.txt
+
+python3 main.py
---
+++ online_store_with_chatbot/chatbot_v2/main.py
@@ -0,0 +1,89 @@
+import torch
+import ollama
+from flask import Flask, request, jsonify
+import csv
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+client = OpenAI()
+
+load_dotenv()
+database_url = os.getenv('OPENAI_API_KEY')
+
+def open_file(filepath):
+    # with open(filepath, 'r', encoding='utf-8') as infile:
+    #    return infile.readlines()
+    content = []
+    with open(filepath, 'r', encoding='utf-8') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            content.append(', '.join(row))
+    return content
+
+
+def generate_embeddings(texts, model_name):
+    embeddings = []
+    for text in texts:
+        response = ollama.embeddings(model=model_name, prompt=text)
+        embeddings.append(response["embedding"])
+    return torch.tensor(embeddings)
+
+
+def get_relevant_context(vault_content, user_input, model_name, embeddings, top_k=3):
+    if embeddings.nelement() == 0:
+        return []
+
+    input_embedding = ollama.embeddings(model=model_name, prompt=user_input)["embedding"]
+    input_embedding_tensor = torch.tensor(input_embedding).unsqueeze(0)
+
+    cos_scores = torch.cosine_similarity(input_embedding_tensor, embeddings)
+    top_k = min(top_k, len(cos_scores))
+    top_indices = torch.topk(cos_scores, k=top_k)[1].tolist()
+
+    relevant_context = [vault_content[idx].strip() for idx in top_indices]
+
+    return relevant_context
+
+
+def gpt_chat(user_input, relevant_context):
+    context_str = "\n".join(relevant_context)
+    messages = [
+        {"role": "system", "content": f"You are a helpful assistant in an online shop. Try to help the user find the best product, using this context:\n{context_str}"},
+        {"role": "user", "content": user_input}
+    ]
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        stream=False,
+    )
+    return response
+
+
+app = Flask(__name__)
+
+context_model = "mxbai-embed-large"
+file_name = "products.csv"
+file_content = open_file(file_name)
+file_embeddings = generate_embeddings(file_content, context_model)
+
+
+@app.route('/query', methods=['POST'])
+def query():
+    data = request.json
+    user_input = data.get('query')
+
+    if not user_input:
+        return jsonify({"error": "No query provided"}), 400
+    
+    print(f"User input {user_input}")
+    relevant_context = get_relevant_context(file_content, user_input, context_model, file_embeddings)
+    print(f"Relevant content {relevant_context}")
+    response = gpt_chat(user_input, relevant_context)
+    print(f"Response {response}")
+    return jsonify({"response": response})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
+
